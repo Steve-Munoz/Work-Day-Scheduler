262a0284cfbe00ccdc09b4f50972802a51ce389e
Steve Munoz <stevomunoz@yahoo.com>
2020-05-21 22:23:11 -0500

    created psuedocode for index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
+// ```
+
+//  I will first make sure javascript doesn't run until the HTML is finished loading
+
+// using moment.js display current time
+//console.log(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+// create an array to add into html
+
+// create a for loop to iterate through my timeblock array and add all my timeblocks to the webpage
+
+// Declare variables for the elements you want to add in your container html
+
+// add classes to each element on html page
+
+// append your new Elements with their attached classes to html page
+
+// I will write my functions below
+
+//  Save to local storage once a button is clicked
+
+// create a function to make sure your text in time block is saved and displayed on the webpage.
