0a06259b33e4a2af456439943ffce831b0466280
Steve Munoz <stevomunoz@yahoo.com>
2020-05-22 16:59:12 -0500

    fixed save button and all my functions and webpage are working correctly

--- index.js
+++ index.js
@@ -13,24 +13,17 @@
 // THEN the saved events persist
 // ```
 
-//var words;
-//var hourInfo;
-
 //  I will first make sure javascript doesn't run until the HTML is finished loading
 
 $(document).ready(function () {
   // using moment.js
   //console.log(moment().format("MMMM Do YYYY, h:mm:ss a"));
-  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-
-  // addtext();
-
-  // const m = moment();
+  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
 
   // creating my array to add into html
   var timeBlocks = [
-    "9:00AM",
-    "10:00AM",
+    "9" + ":00AM",
+    "10" + ":00AM",
     "11:00AM",
     "12:00PM",
     "1:00PM",
@@ -55,8 +48,10 @@ $(document).ready(function () {
     addTextArea.addClass("textarea col-8");
     // this below took me a while to figure out. I'ts still not doing what I want it to do
     //It's assgining each of my timeblocks an ID with the number of the index it's currently
-    //.. on in my for loop
-    addTextArea.attr("data-hour", timeBlocks.indexOf(timeBlocks[i]));
+    //.. on in my for loop ---UPDATE: I fixed it! on line 54.
+
+    // addTextArea.attr("id", timeBlocks.indexOf(timeBlocks[i]));
+    addTextArea.attr("id", timeBlocks[i]);
     addSaveButton.addClass("saveBtn col-2");
     addSaveHover.addClass("far fa-save");
 
@@ -72,6 +67,22 @@ $(document).ready(function () {
     addSaveButton.append(addSaveHover);
   }
 
+  //  Save to local storage once a button is clicked
+
+  $(".saveBtn").on("click", function () {
+    // line 74 and 75 do the same thing as line 76
+    //var textarea1 = $(this).siblings(".textarea");
+    // var words = textarea1.val();
+    var words = $(this).siblings(".textarea").val();
+    console.log(words);
+    var hourInfo = $(this).siblings(".hour").text();
+    console.log(hourInfo);
+    localStorage.setItem(hourInfo, JSON.stringify(words));
+
+    // blockColor();
+    // renderText();
+  });
+
   // call Functions
   blockColor();
   renderText();
@@ -83,7 +94,6 @@ $(document).ready(function () {
 // Each block will be color coded to indicate if the block that I am currently in
 // is in the past present or future
 function blockColor() {
-  //var timeTest = addTextArea.val();
   var currentTime = moment().format("h");
   console.log(currentTime);
   $(".textarea").each(function () {
@@ -107,56 +117,42 @@ function blockColor() {
   });
 }
 
-//  Save to local storage once a button is clicked
-//  I do not know why this below is not working, I feel like the code is right
-//
-
-$(".saveBtn").on("click", function () {
-  var textarea = $(".textarea");
-  var words = $(this).siblings(textarea).val();
-  console.log(words);
-  var hourInfo = $(this).siblings(".hour").text();
-  console.log(hourInfo);
-  localStorage.setItem(hourInfo, JSON.stringify(words));
-
-  blockColor();
-  renderText();
-});
-
 // create a function to make sure your text in time block is saved and displayed on the webpage
 
 function renderText() {
-  var saveWords9 = JSON.parse(localStorage.getItem("9:00AM"));
+  var saveText9 = JSON.parse(localStorage.getItem("9:00AM"));
 
-  $("#0").val(saveWords9);
+  $("#9").val(saveText9);
 
-  var saveWords10 = JSON.parse(localStorage.getItem("10:00AM"));
+  var saveText10 = JSON.parse(localStorage.getItem("10:00AM"));
 
-  $("#1").val(saveWords10);
+  $("#10").val(saveText10);
 
-  var saveWords11 = JSON.parse(localStorage.getItem("11:00AM"));
+  var saveText11 = JSON.parse(localStorage.getItem("11:00AM"));
 
-  $("#2").val(saveWords11);
+  $("#11").val(saveText11);
 
-  var saveWords12 = JSON.parse(localStorage.getItem("12:00PM"));
+  var saveText12 = JSON.parse(localStorage.getItem("12:00PM"));
 
-  $("#3").val(saveWords12);
+  $("#12").val(saveText12);
 
-  var saveWords1 = JSON.parse(localStorage.getItem("1:00PM"));
+  var saveText1 = JSON.parse(localStorage.getItem("1:00PM"));
 
-  $("#4").val(saveWords1);
+  $("#1").val(saveText1);
 
-  var saveWords2 = JSON.parse(localStorage.getItem("2:00PM"));
+  var saveText2 = JSON.parse(localStorage.getItem("2:00PM"));
 
-  $("#5").val(saveWords2);
+  $("#2").val(saveText2);
 
-  $("#6").val(saveWords3);
+  var saveText3 = JSON.parse(localStorage.getItem("3:00PM"));
 
-  var saveWords4 = JSON.parse(localStorage.getItem("4:00PM"));
+  $("#3").val(saveText3);
 
-  $("#7").val(saveWords4);
+  var saveText4 = JSON.parse(localStorage.getItem("4:00PM"));
 
-  var saveWords5 = JSON.parse(localStorage.getItem("5:00PM"));
+  $("#4").val(saveText4);
 
-  $("#8").val(saveWords5);
+  var saveText5 = JSON.parse(localStorage.getItem("5:00PM"));
+
+  $("#5").val(saveText5);
 }
